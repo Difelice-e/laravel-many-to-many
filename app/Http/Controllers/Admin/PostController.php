f078429477cd56c3ceb45fa71e95fc7f47bf90a1
Difelice-e <difelice.emanuele@gmail.com>
2022-05-05 23:38:26 +0200

    creata repo + creati tag model, seeder, migration + ho scordato di pushare

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Post;
+use App\Category;
+use App\Tag;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::with(['category','tags'])->orderBy('created_at','desc')->limit(20)->get();
+
+        return view('admin.posts.index',compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+
+        return view('admin.posts.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:150',
+            'content' => 'required',
+            'published_at' => 'nullable|date|before_or_equal:today',
+            'category_id' => 'nullable|exists:categories,id'
+        ]);
+
+        $data = $request->all();
+
+        $slug = Post::getUniqueSlug($data['title']);
+
+        $post = new Post();
+        $post->fill($data);
+        $post->slug = $slug;
+
+        $post->save();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+
+        return view('admin.posts.edit',compact('post','categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|max:150',
+            'content' => 'required',
+            'published_at' => 'nullable|date|before_or_equal:today',
+            'category_id' => 'nullable|exists:categories,id',
+        ]);
+
+        $data = $request->all();
+
+        if ($post->title != $data['title']) {
+            $slug = Post::getUniqueSlug($data['title']);
+            $data['slug'] = $slug;
+        }
+
+        
+
+        $post->update($data);
+        
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'published_at',
+        'slug',
+        'category_id',
+    ];
+
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function tags() {
+        return $this->belongsToMany('App\Tag');
+    }
+
+    public static function getUniqueSlug($title) {
+        $slug = Str::slug($title);
+        $slug_base = $slug;
+        
+        $counter = 1;
+
+        $post_present = Post::where('slug',$slug)->first();
+        
+        while ($post_present) {
+            $slug = $slug_base . '-' . $counter;
+            $counter++;
+            $post_present = Post::where('slug',$slug)->first();
+        }
+
+        return $slug;
+    }
+}
